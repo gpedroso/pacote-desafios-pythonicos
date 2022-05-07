3466a0fc732c3be8e350959f2d5f42a0e02dace5
gpedroso <gpedroso@gmail.com>
2022-05-07 06:42:00 -0300

    regex solution 06_not_bad

--- 06_not_bad.py
+++ 06_not_bad.py
@@ -8,9 +8,19 @@ por 'good' e retorne a string resultante.
 
 Exemplo: 'The dinner is not that bad!' retorna 'The dinner is good!'
 """
+from operator import indexOf
+import re
+
 
 def not_bad(s):
     # +++ SUA SOLUÇÃO +++
+    return re.sub('not( [0-9a-zA-Z]*)? bad', 'good', s) 
+
+
+def not_bad_firstSolution(s):
+    # +++ SUA SOLUÇÃO +++
+
+
     indexNot = s.find('not')
     indexBad = s.find('bad')
 
@@ -46,8 +56,12 @@ def test(f, in_, expected):
 
 if __name__ == '__main__':
     # Testes que verificam o resultado do seu código em alguns cenários.
-    test(not_bad, 'This movie is not so bad', 'This movie is good')
+    test(not_bad, 'This movie is not 123 bad', 'This movie is good')
     test(not_bad, 'This dinner is not that bad!', 'This dinner is good!')
     test(not_bad, 'This tea is not hot', 'This tea is not hot')
     test(not_bad, "It's bad yet not", "It's bad yet not")
     test(not_bad, 'He is not bad', 'He is good')
+    test(not_bad, 'This movie is not good and the director is not so bad', 'This movie is not good and the director is good')
+    test(not_bad, 'This movie is not bad and the director is not so bad', 'This movie is good and the director is good')
+
+
